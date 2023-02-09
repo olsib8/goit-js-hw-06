8e9545f92d601d974e11bafea342b70f7b4dee05
olsib8 <alsib8@gmail.com>
2023-02-09 12:35:18 +0200

    task-1

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+const listArray = document.querySelectorAll('.item');
+console.log(`Number of categories: ${listArray.length}`);
+
+const categoryAndSum = listArray.forEach((el) => {
+    console.log(`Category: ${el.firstElementChild.textContent}`);
+    console.log(`Elements: ${el.lastElementChild.children.length}`);
+})

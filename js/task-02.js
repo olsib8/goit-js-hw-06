89db275c912bf0406f2171b360fcf8dad1569c27
olsib8 <alsib8@gmail.com>
2023-02-09 17:20:39 +0200

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulEl = document.querySelector('#ingredients');
+
+ingredients.forEach((el) => {
+  const liEl = document.createElement('li');
+  liEl.textContent = el;
+  liEl.classList = "item";
+  ulEl.append(liEl);
+})

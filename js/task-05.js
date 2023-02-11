44f60906c2a79d7cd8cef264236ec56430d7531d
olsib8 <alsib8@gmail.com>
2023-02-11 23:15:20 +0200

    task-5

---
+++ js/task-05.js
@@ -0,0 +1,14 @@
+const textInput = document.querySelector("#name-input");
+const textOutput = document.querySelector("#name-output");
+
+const handleInput = (event) => {
+    if (!event.currentTarget.value) {
+        textOutput.textContent = "Anonymous";
+    } else {
+        textOutput.textContent = event.currentTarget.value.trim();
+    }
+    };
+
+textInput.addEventListener("input", handleInput);
+
+

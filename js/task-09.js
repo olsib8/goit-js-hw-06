d46d582335764aff20b6b8607d1b6cc916788abe
olsib8 <alsib8@gmail.com>
2023-02-14 23:31:18 +0200

    task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const buttonColor = document.querySelector(".change-color");
+const bodyColor = document.querySelector("body");
+const spanColor = document.querySelector(".color");
+buttonColor.addEventListener("click", changeColor);
+
+function changeColor(event) {
+  bodyColor.style.backgroundColor = getRandomHexColor();
+  spanColor.textContent = getRandomHexColor();
 }

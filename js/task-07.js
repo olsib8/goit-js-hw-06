3453284e98bc7361ff939e258500fe02bae5db0c
olsib8 <alsib8@gmail.com>
2023-02-14 12:48:45 +0200

    task-7

---
+++ js/task-07.js
@@ -0,0 +1,10 @@
+const inputChange = document.querySelector('#font-size-control');
+const spanText = document.querySelector('#text');
+
+inputChange.addEventListener('input', onTextChange);
+function onTextChange (event) {
+    spanText.style.fontSize = `${event.currentTarget.value}px`;
+    }
+
+
+
